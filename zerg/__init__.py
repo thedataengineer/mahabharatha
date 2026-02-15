a8e30d595005d272cc903b8ebe9d226632b5ffb9
ZERG Worker 0 <zerg-worker@local>
2026-02-15 08:26:50 -0700

    chore(release): bump version to 0.3.0
    Minor version bump for security engine integration release.

--- zerg/__init__.py
+++ zerg/__init__.py
@@ -3,7 +3,7 @@
 Overwhelm features with coordinated worker instances.
 """
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __author__ = "ZERG Team"
 
 from zerg.architecture import ArchitectureChecker, ArchitectureConfig
